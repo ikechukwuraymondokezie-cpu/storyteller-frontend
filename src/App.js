1038dd9d41459b6122e2d77091b7ec5c7ba26d5a
ikechukwuraymondokezie-cpu <ikechukwuraymondokezie@gmail.com>
2026-01-13 20:23:57 +0100

    deskstop racism

--- src/App.js
+++ src/App.js
@@ -1,17 +1,24 @@
+import React from "react";
 import TopNav from "./components/TopNav";
 import Storyteller from "./components/Storyteller";
 import BottomNav from "./components/BottomNav";
+import FloatingUploadButton from "./components/FloatingUploadButton";
 
 function App() {
   return (
-    <>
+    <div className="App">
       <TopNav />
-      <Storyteller />
+      <main className="pt-20">
+        <Storyteller />
+      </main>
+
+      {/* Mobile bottom navbar */}
       <BottomNav />
-    </>
+
+      {/* Desktop floating upload button */}
+      <FloatingUploadButton />
+    </div>
   );
 }
 
 export default App;
-
-
--- src/components/BottomNav.js
+++ src/components/BottomNav.js
@@ -11,27 +11,21 @@ function NavItem({ icon, label }) {
 
 export default function BottomNav() {
     return (
-        <nav className="fixed bottom-0 left-0 right-0 h-16 bg-black border-t border-gray-800 z-50">
-            <div className="flex justify-around items-center h-full">
+        <nav className="fixed bottom-0 left-0 right-0 h-16 bg-black border-t border-gray-800 z-50 flex justify-around items-center md:hidden">
+            <NavItem icon={<Home className="w-5 h-5" />} label="Home" />
+            <NavItem icon={<BookOpen className="w-5 h-5" />} label="Library" />
 
-                {/* Left items */}
-                <NavItem icon={<Home className="w-5 h-5" />} label="Home" />
-                <NavItem icon={<BookOpen className="w-5 h-5" />} label="Library" />
+            {/* Upload button */}
+            <button
+                className="w-12 h-12 bg-gradient-to-r from-red-500 via-orange-400 to-yellow-300 
+                   flex items-center justify-center text-white text-xl rounded-full 
+                   shadow-[0_0_12px_rgba(255,140,0,0.7)] hover:scale-105 transition-transform"
+            >
+                <Plus className="w-5 h-5" />
+            </button>
 
-                {/* Upload button – square and full navbar height */}
-                <button
-                    className="w-16 h-full bg-gradient-to-r from-red-500 via-orange-400 to-yellow-300 
-                     flex items-center justify-center text-white text-xl rounded-none 
-                     shadow-[0_0_12px_rgba(255,140,0,0.7)] hover:scale-105 transition-transform"
-                >
-                    <Plus className="w-6 h-6" />
-                </button>
-
-                {/* Right items */}
-                <NavItem icon={<Flame className="w-5 h-5" />} label="F3" />
-                <NavItem icon={<User className="w-5 h-5" />} label="Profile" />
-
-            </div>
+            <NavItem icon={<Flame className="w-5 h-5" />} label="F3" />
+            <NavItem icon={<User className="w-5 h-5" />} label="Profile" />
         </nav>
     );
 }
