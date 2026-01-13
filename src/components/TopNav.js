b67782d46deeca86cfc39ef54cf2571489e0f605
ikechukwuraymondokezie-cpu <ikechukwuraymondokezie@gmail.com>
2026-01-13 13:44:49 +0100

    Add TopNav.js matching F3 navbar style

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -6,7 +6,7 @@ export default function TopNav() {
         <nav className="fixed top-0 left-0 w-full h-16 bg-black z-50 border-b border-gray-800">
             <div className="max-w-7xl mx-auto h-full px-6 flex items-center">
                 {/* Logo */}
-                <img src={Logo} alt="Storyteller logo" className="h-10 w-auto" />
+                <img src={Logo} alt="Storyteller logo" className="h-7 w-auto" />
             </div>
         </nav>
     );
