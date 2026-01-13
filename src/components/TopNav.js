c4e6bdce72776471d2db07cec70c7217b874729a
ikechukwuraymondokezie-cpu <ikechukwuraymondokezie@gmail.com>
2026-01-13 13:34:58 +0100

    Add TopNav.js matching F3 navbar style

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,46 +1,13 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import logo from "../assets/logo.png";
+import Logo from "../assets/logo.png";
 
 export default function TopNav() {
     return (
-        <header className="fixed top-0 left-0 right-0 h-16 bg-black border-b border-gray-800 z-50">
-            <div className="max-w-7xl mx-auto h-full px-6 flex items-center justify-between">
+        <nav className="fixed top-0 left-0 w-full h-16 bg-black z-50 border-b border-gray-800">
+            <div className="max-w-7xl mx-auto h-full px-6 flex items-center">
                 {/* Logo */}
-                <div className="flex items-center gap-2">
-                    <img src={logo} alt="Storyteller logo" className="h-8" />
-                    <span className="text-white text-xl font-semibold">StoryTeller</span>
-                </div>
-
-                {/* Navigation Links */}
-                <nav className="flex gap-6 text-white text-lg">
-                    <Link
-                        to="/"
-                        className="hover:text-yellow-400 transition-colors duration-200"
-                    >
-                        Home
-                    </Link>
-                    <Link
-                        to="/stories"
-                        className="hover:text-yellow-400 transition-colors duration-200"
-                    >
-                        Stories
-                    </Link>
-                    <Link
-                        to="/upload"
-                        className="hover:text-yellow-400 transition-colors duration-200"
-                    >
-                        Upload
-                    </Link>
-                    <Link
-                        to="/about"
-                        className="hover:text-yellow-400 transition-colors duration-200"
-                    >
-                        About
-                    </Link>
-                </nav>
+                <img src={Logo} alt="Storyteller logo" className="h-10 w-auto" />
             </div>
-        </header>
+        </nav>
     );
 }
-
