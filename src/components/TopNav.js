4688f84ed6dc40a8425515d23f1c6a54dab4acae
ikechukwuraymondokezie-cpu <ikechukwuraymondokezie@gmail.com>
2026-01-13 13:53:49 +0100

    Add TopNav.js matching F3 navbar style

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -1,13 +1,21 @@
 import React from "react";
-import Logo from "../assets/logo.png";
+import Logo from "../assets/logo.png"; // your single logo.png
 
 export default function TopNav() {
     return (
-        <nav className="fixed top-0 left-0 w-full h-16 bg-black z-50 border-b border-gray-800">
+        <nav
+            className="fixed top-0 left-0 w-full h-16 bg-black z-50 border-b border-gray-800"
+            aria-label="Main navigation"
+        >
             <div className="max-w-7xl mx-auto h-full px-6 flex items-center">
-                {/* Logo */}
-                <img src={Logo} alt="Storyteller logo" className="h-7 w-auto" />
+                {/* Logo only */}
+                <img
+                    src={Logo}
+                    alt="Storyteller logo"
+                    className="h-[56px] w-auto transition-all duration-300"
+                />
             </div>
         </nav>
     );
 }
+
