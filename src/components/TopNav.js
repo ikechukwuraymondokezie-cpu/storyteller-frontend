3528311278c3cdaf73950b39b09f1bbac2286807
ikechukwuraymondokezie-cpu <ikechukwuraymondokezie@gmail.com>
2026-01-17 15:13:10 +0100

    :L:

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -9,22 +9,22 @@ export default function TopNav() {
     return (
         <nav
             className="
-        fixed z-50
-        top-0 left-0
-        w-full h-11
-        md:w-32 md:h-screen
-        bg-transparent
-        md:bg-black/40 md:backdrop-blur
-        border-b-0 md:border-r md:border-white/10
-      "
+                fixed z-50
+                top-0 left-0
+                w-full h-14
+                md:w-32 md:h-screen
+                bg-transparent
+                md:bg-black/40 md:backdrop-blur
+                border-b-0 md:border-r md:border-white/10
+            "
         >
             <div
                 className="
-          flex h-full items-center justify-between px-4
-          md:flex-col md:justify-start md:px-0 md:py-8 md:gap-12
-        "
+                    flex h-full items-center justify-between px-4
+                    md:flex-col md:justify-start md:px-0 md:py-8 md:gap-12
+                "
             >
-                {/* Logo OR Library Text */}
+                {/* LOGO OR LIBRARY TEXT */}
                 <div className="flex items-center justify-center">
                     {isLibrary ? (
                         <span className="text-white font-semibold text-2xl md:text-xl">
@@ -34,12 +34,17 @@ export default function TopNav() {
                         <img
                             src={logo}
                             alt="Storyteller"
-                            className="h-13 w-auto md:h-10 md:w-auto"
+                            className="
+                                h-10
+                                md:h-14
+                                w-auto
+                                object-contain
+                            "
                         />
                     )}
                 </div>
 
-                {/* NAV LINKS (desktop only) */}
+                {/* DESKTOP NAV LINKS */}
                 <div className="hidden md:flex flex-col items-center gap-8 text-sm text-white">
                     <a className="hover:text-yellow-400 transition" href="/">
                         Home
@@ -52,7 +57,7 @@ export default function TopNav() {
                     </a>
                 </div>
 
-                {/* Mobile icons (Library page only) */}
+                {/* MOBILE ICONS (Library page only) */}
                 {isLibrary && (
                     <div className="flex items-center gap-4 md:hidden text-white">
                         <Search className="w-5 h-5 cursor-pointer" />
