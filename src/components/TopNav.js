692260c5707b40ed76a7fd9d3e9c8db0023c4541
ikechukwuraymondokezie-cpu <ikechukwuraymondokezie@gmail.com>
2026-01-17 23:42:00 +0100

    ;

--- src/components/TopNav.js
+++ src/components/TopNav.js
@@ -35,7 +35,7 @@ export default function TopNav() {
                             <img
                                 src={logo}
                                 alt="Storyteller"
-                                className="h-14 w-auto object-contain"
+                                className="h-15 w-auto object-contain"
                             />
                         </Link>
                     )}
