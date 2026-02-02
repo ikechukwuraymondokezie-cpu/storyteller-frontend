040e42dcf422bb84e97d318a7f9ab1ef6c576a62
ikechukwuraymondokezie-cpu <ikechukwuraymondokezie@gmail.com>
2026-02-02 15:06:59 +0100

    :

--- storyteller-frontend/src/App.js
+++ storyteller-frontend/src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import TopNav from "./components/TopNav";
 import BottomNav from "./components/BottomNav";
@@ -6,21 +6,27 @@ import FloatingUploadButton from "./components/FloatingUploadButton";
 
 import Storyteller from "./components/Storyteller";
 import Library from "./components/Library";
+import Profile from "./components/Profile"; // 1. Import your new Profile component
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <div className="App min-h-screen flex flex-col md:flex-row bg-bg">
 
         {/* Sidebar / TopNav */}
         <TopNav />
 
         {/* Main content */}
-        {/* Reverted back to pt-11 to protect your logo/header spacing */}
         <main className="flex-1 pt-11 md:pt-6 pb-32 md:ml-32 overflow-auto">
           <Routes>
             <Route path="/" element={<Storyteller />} />
             <Route path="/library" element={<Library />} />
+
+            {/* 2. Add the Profile Route */}
+            <Route path="/profile" element={<Profile />} />
+
+            {/* Catch-all redirect (prevents blank screen) */}
+            <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </main>
 
@@ -29,10 +35,10 @@ function App() {
           <BottomNav />
         </div>
 
-        {/* Desktop floating upload button */}
+        {/* Floating upload button */}
         <FloatingUploadButton />
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
