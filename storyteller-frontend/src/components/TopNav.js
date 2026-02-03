13af61168a48e5cc1804e2ab18ce784381707def
ikechukwuraymondokezie-cpu <ikechukwuraymondokezie@gmail.com>
2026-02-03 18:55:16 +0100

    :

--- storyteller-frontend/src/components/TopNav.js
+++ storyteller-frontend/src/components/TopNav.js
@@ -103,7 +103,7 @@ export default function TopNav() {
                         ) : isProfile ? (
                             <span className="text-white font-semibold text-lg">Profile</span>
                         ) : (
-                            <img src={logo} alt="Storyteller" className="h-10" />
+                            <img src={logo} alt="Storyteller" className="h-8" />
                         )}
                     </div>
 
