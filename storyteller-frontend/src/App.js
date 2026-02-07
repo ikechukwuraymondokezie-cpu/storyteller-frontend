b55ae502919bcc502c151f179400b8348c312a5e
ikechukwuraymondokezie-cpu <ikechukwuraymondokezie@gmail.com>
2026-02-07 06:55:12 +0100

    :

--- storyteller-frontend/src/App.js
+++ storyteller-frontend/src/App.js
@@ -7,38 +7,44 @@ import FloatingUploadButton from "./components/FloatingUploadButton";
 import Storyteller from "./components/Storyteller";
 import Library from "./components/Library";
 import Profile from "./components/Profile";
-import Reader from "./components/Reader"; // Import the Reader
+import Reader from "./components/Reader";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App min-h-screen flex flex-col md:flex-row bg-bg">
 
-        {/* Sidebar / TopNav */}
+        {/* Sidebar / TopNav - Visible on Desktop/Mobile */}
         <TopNav />
 
-        {/* Main content */}
+        {/* Main content area */}
         <main className="flex-1 pt-11 md:pt-6 pb-32 md:ml-32 overflow-auto">
           <Routes>
+            {/* Home/Feed View */}
             <Route path="/" element={<Storyteller />} />
+
+            {/* User Library View */}
             <Route path="/library" element={<Library />} />
+
+            {/* User Profile View */}
             <Route path="/profile" element={<Profile />} />
 
-            {/* 1. Add the Reader Route */}
-            {/* Using :id so you can load specific books */}
+            {/* Dynamic Reader Route: 
+               The :id allows Reader.js to use useParams() to fetch the correct PDF 
+            */}
             <Route path="/reader/:id" element={<Reader />} />
 
-            {/* Catch-all redirect */}
+            {/* Catch-all redirect to Home */}
             <Route path="*" element={<Navigate to="/" replace />} />
           </Routes>
         </main>
 
-        {/* Mobile bottom navbar */}
+        {/* Mobile bottom navbar - Hidden on desktop */}
         <div className="md:hidden">
           <BottomNav />
         </div>
 
-        {/* Floating upload button */}
+        {/* Global Floating upload button */}
         <FloatingUploadButton />
       </div>
     </BrowserRouter>
