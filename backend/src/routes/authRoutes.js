0ca04e62c04b7197cd879cd57856b13168daeee2
ikechukwuraymondokezie-cpu <ikechukwuraymondokezie@gmail.com>
2026-02-24 12:54:41 +0100

    :

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { register, login } = require('../controllers/authController');
+// Changed 'register' to 'registerUser' and 'login' to 'loginUser'
+const { registerUser, loginUser } = require('../controllers/authController');
 
 // URL: /api/auth/register
-router.post('/register', register);
+router.post('/register', registerUser);
+
 // URL: /api/auth/login
-router.post('/login', login);
+router.post('/login', loginUser);
 
 module.exports = router;
